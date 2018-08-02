372106c7a1b14d52e05f5773347efb78f1bd4723
samuraimac <39005419+samuraimac@users.noreply.github.com>
2018-08-02 14:04:35 +0800

    Update SeedStatus.java

--- View/SeedStatus.java
+++ View/SeedStatus.java
@@ -10,10 +10,6 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 import javax.swing.border.Border;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
