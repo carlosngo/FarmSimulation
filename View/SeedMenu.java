ee47d7c1ba9e28afc312d8415d47c0b8cf047d59
Carlos <carlos_ngo@dlsu.edu.ph>
2018-08-09 11:54:33 +0800

    Add files via upload

--- View/SeedMenu.java
+++ View/SeedMenu.java
@@ -19,122 +19,130 @@ import javax.swing.border.Border;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class SeedMenu extends JFrame implements ActionListener {
-
-    private JButton[][] actionButtons;
     private JButton back;
-    private JTabbedPane tp;
-    private TabPanel pVeggie, pFlower, pTree;
-    private GridBagConstraints c = new GridBagConstraints();
-    private static int row;
-    private static int col;
+    private JPanel informationPanel, seedPanel;
     private GameGUIController controller;
+    private JLabel pic, name, ht, wn, fn, sc, hc, pp, bp;
 
-    public TabPanel getpVeggie() {
-        return pVeggie;
+    public void setPic(BufferedImage img) {
+        pic.setIcon(new ImageIcon(img));
+    }
+    
+    public void setName(String s) {
+        this.name.setText("Name: " + s);
     }
 
-    public TabPanel getpFlower() {
-        return pFlower;
+    public void setHt(String s) {
+        ht.setText("Harvest Time: " + s);
     }
 
-    public TabPanel getpTree() {
-        return pTree;
+    public void setWn(String s) {
+        wn.setText("Water Needed (max): " + s);
     }
 
-    public JFrame getFrame() {
-        return this;
+    public void setFn(String s) {
+        fn.setText("Fertilizer Needed (max): " + s);
     }
 
+    public void setSc(String s) {
+        sc.setText("Seed Cost: " + s);
+    }
+
+    public void setHc(String s) {
+        hc.setText("Harvest Cost: " + s);
+    }
+
+    public void setPp(String s) {
+        pp.setText("Products Produced: " + s);
+    }
+
+    public void setBp(String s) {
+        bp.setText("Base Price: " + s);
+    }
+    
     public SeedMenu(GameGUIController controller) {
         this.controller = controller;
-        row = 0;
-        col = 0;
-        actionButtons = new JButton[12][6];
         initSeedMenu();
     }
 
     public void initSeedMenu() {
-        JPanel p = new JPanel();
-        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-        p.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        tp = new JTabbedPane();
-        tp.setFont(new Font("Abril Fatface", Font.PLAIN, 14));
-
-        pVeggie = new TabPanel("Vegetables");
-        tp.add("Vegetables", pVeggie);
-
-        pFlower = new TabPanel("Flowers");
-        tp.add("Flowers", pFlower);
-
-        pTree = new TabPanel("Trees");
-        tp.add("Fruit Tree", pTree);
+        JPanel content = new JPanel();
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+        setContentPane(content);        
+        informationPanel = new JPanel();
+        informationPanel.setLayout(new BorderLayout());
+        JPanel seedInfo = new JPanel();
+        seedInfo.setLayout(new GridLayout(4, 2));
         
-        back = new JButton("Back");
-        back.setFont(new Font("Abril Fatface", Font.PLAIN, 18));
-        back.setAlignmentX(Component.CENTER_ALIGNMENT);
-        back.addActionListener(this);
-
-        JTextArea legend = new JTextArea(3, 50);
-        legend.setText("Legend:\nSN – seed name; CT – crop type; HT – time to harvest in minutes; WN (bl) – water needed (bonus limit); FN (bl) – "
-                + "Fertilizer needed (bonus limit);\nHC – harvest cost; PP – number of products produced; SC – seed cost; BP – base selling"
-                + " cost per piece produced");
-        legend.setFont(new Font("Times New Roman", Font.BOLD, 14));
-        legend.setAlignmentX(Component.CENTER_ALIGNMENT);
-        legend.setLineWrap(true);
-        legend.setWrapStyleWord(true);
-        legend.setOpaque(false);
-        legend.setEditable(false);
-        JScrollPane sp = new JScrollPane(legend);
-        sp.setBorder(BorderFactory.createEmptyBorder());
+        name = new JLabel("Name: ");
+        name.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(name);
+        
+        ht = new JLabel("Harvest Time: ");        
+        ht.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(ht);
+        
+        wn = new JLabel("Water Needed (max): ");        
+        wn.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(wn);
+        
+        fn = new JLabel("Fertilizer Needed (max): ");        
+        fn.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(fn);
+        
+        hc = new JLabel("Harvest Cost: ");        
+        hc.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(hc);
+        
+        pp = new JLabel("Products Produced: ");        
+        pp.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(pp);
+        
+        sc = new JLabel("Seed Cost: ");        
+        sc.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(sc);
+        
+        bp = new JLabel("Base Price: ");        
+        bp.setHorizontalAlignment(SwingConstants.CENTER);
+        seedInfo.add(bp);
+        
+        informationPanel.add(pic, BorderLayout.WEST);
+        informationPanel.add(seedInfo, BorderLayout.EAST);
+        
+        seedPanel = new JPanel();
+        JScrollPane sp = new JScrollPane(seedPanel);
+//        sp.setBorder(BorderFactory.createEmptyBorder());
         JLabel title = new JLabel("Seed List");
         title.setFont(new Font("Abril Fatface", Font.BOLD, 24));
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
-        p.add(title);
-        p.add(tp);
-        p.add(Box.createRigidArea(new Dimension(0, 10)));
-        p.add(sp);
-        p.add(Box.createRigidArea(new Dimension(0, 10)));
-        p.add(back);
-        setContentPane(p);
+        content.add(title);
+        content.add(informationPanel);
+        content.add(Box.createRigidArea(new Dimension(0, 10)));
+        content.add(sp);
+        content.add(Box.createRigidArea(new Dimension(0, 10)));
+        content.add(back);
+        setContentPane(content);
         setSize(1100, 420);
         setResizable(false);
         setUndecorated(true);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
-        setVisible(true);
+        setVisible(false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
-    public void addActionListeners() {
-        for (ArrayList<JButton> list : pVeggie.getActionButtons()) {
-            for (JButton b : list)
-                b.addActionListener(this);
-        }
-        
-        for (ArrayList<JButton> list : pFlower.getActionButtons()) {
-            for (JButton b : list)
-                b.addActionListener(this);
-        }
-        
-        for (ArrayList<JButton> list : pTree.getActionButtons()) {
-            for (JButton b : list)
-                b.addActionListener(this);
-        }
+    public void addSeed(String name) {
+        JButton btn = new JButton();
+        btn.setIcon(new ImageIcon(controller.getPlantImage(name)));
+        btn.setActionCommand(name);
+        btn.addActionListener(this);
+        seedPanel.add(btn);
     }
-
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == back) {
-            dispose();
+            setVisible(false);
         } else {
-            TabPanel pnl = (TabPanel) tp.getSelectedComponent();
-            for (int i = 0; i < pnl.getActionButtons().size(); i++) {
-                for (int j = 0; j < 2; j++) {
-                    if (e.getSource().equals(pnl.getActionButtons().get(i).get(j))) {
-                          controller.updateSelected((JButton) e.getSource(), pnl.getNames().get(i), 0);
-                        
-                    }
-                }
-            }
+            updateSeedInfo(e.getActionCommand());
         }
     }
 
